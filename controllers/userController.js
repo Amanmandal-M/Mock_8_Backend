a66252839bc546bebb555a7c66fcbc166d050e90
Amanmandal-M <amanmandal644@gmail.com>
2023-06-03 19:26:30 +0530

    Updated Backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,7 +61,7 @@ const loginController = async (req,res) => {
                 
             const Normal_Token = jwt.sign({"masai":"masai"}, process.env.NORMAL_KEY,{expiresIn:"7d"});
                     
-            res.cookie("Normal_Token", Normal_Token);
+            res.cookie("Normal_Token", Normal_Token ,{ httpOnly: true });
     
             res.status(201).send({
                 "Message":"Login successful",
