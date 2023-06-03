2ac6805b735128e0db5ce3217422a2b32d514075
Amanmandal-M <amanmandal644@gmail.com>
2023-06-03 21:21:11 +0530

    Remove cookie from backend

--- middlewares/authenticationMiddleware.js
+++ middlewares/authenticationMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authentication = async (req,res,next) => {
     try {
-        const normalToken = req.cookies.Normal_Token || "";
+        const normalToken = req.headers.Normal_Token || "";
 
         // Verify the token
         jwt.verify(normalToken, process.env.NORMAL_KEY, (err, decoded) => {
